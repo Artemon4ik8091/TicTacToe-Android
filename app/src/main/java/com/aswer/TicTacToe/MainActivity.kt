056c888783477bc8ddbef2b48e8c18362899872c
aswer <aswert092@gmail.com>
2025-06-11 15:35:11 +0300

    add_secrets

--- app/src/main/java/com/aswer/TicTacToe/MainActivity.kt
+++ app/src/main/java/com/aswer/TicTacToe/MainActivity.kt
@@ -3,10 +3,12 @@ package com.aswer.TicTacToe
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +32,13 @@ class MainActivity : AppCompatActivity() {
         val button8: Button = findViewById(R.id.button8)
         val button9: Button = findViewById(R.id.button9)
         val reset: Button = findViewById(R.id.reset)
+        val funText: TextView = findViewById(R.id.fun_text)
+        val podval: TextView = findViewById(R.id.podval)
 
         logo.text = "Крестики – нолики!"
         win.text = " "
         reset.text = "Начать заново."
+        funText.text == " "
 
         button1.text = " "
         button2.text = " "
@@ -67,50 +72,69 @@ class MainActivity : AppCompatActivity() {
             else { win.text = " " }
         }
 
+        fun check_fun() {
+            if (button1.text == "❌" && button5.text == "❌" && button9.text == "❌" && button3.text == "❌" && button7.text == "❌") { funText.text = "SUPER MEGA X" }
+            else if (button2.text == "❌" && button6.text == "❌" && button8.text == "❌" && button4.text == "❌") { funText.text = "РомбЭк." }
+            //else if (button2.text == "❌" && button5.text == "❌" && button8.text == "❌" && button6.text == "❌" && button4.text == "❌") { funText.text = "++++++++++++" }
+            //else if (button1.text == "❌" && button2.text == "❌" && button3.text == "❌" && button6.text == "❌" && button9.text == "❌" && button8.text == "❌" && button7.text == "❌" && button4.text == "❌") { funText.text = "КВАДРАТик" }
+
+            else if (button1.text == "⭕" && button5.text == "⭕" && button9.text == "⭕" && button3.text == "⭕" && button7.text == "⭕") { funText.text = "Just X" }
+            else if (button2.text == "⭕" && button6.text == "⭕" && button8.text == "⭕" && button4.text == "⭕") { funText.text = "РомбЭк." }
+            //else if (button2.text == "⭕" && button5.text == "⭕" && button8.text == "⭕" && button6.text == "⭕" && button4.text == "⭕") { funText.text = "++++++++++++" }
+            //else if (button1.text == "⭕" && button2.text == "⭕" && button3.text == "⭕" && button6.text == "⭕" && button9.text == "⭕" && button8.text == "⭕" && button7.text == "⭕" && button4.text == "⭕") { funText.text = "КВАДРАТик" }
+
+            else { funText.text = " " }
+        }
+
+        fun check_all() {
+            check_win()
+            check_fun()
+        }
+
         button1.setOnClickListener {
             if (button1.text == " " || button1.text == "⭕") { button1.text = "❌" }
             else { button1.text = "⭕" }
-            check_win()
+            check_all()
         }
         button2.setOnClickListener {
             if (button2.text == " " || button2.text == "⭕") { button2.text = "❌" }
             else { button2.text = "⭕" }
-            check_win()
+            check_all()
         }
         button3.setOnClickListener {
             if (button3.text == " " || button3.text == "⭕") { button3.text = "❌" }
             else { button3.text = "⭕" }
-            check_win()
+            check_all()
         }
         button4.setOnClickListener {
             if (button4.text == " " || button4.text == "⭕") { button4.text = "❌" }
             else { button4.text = "⭕" }
-            check_win()
+            check_all()
         }
         button5.setOnClickListener {
             if (button5.text == " " || button5.text == "⭕") { button5.text = "❌" }
             else { button5.text = "⭕" }
-            check_win()
+            check_all()
         }
         button6.setOnClickListener {
             if (button6.text == " " || button6.text == "⭕") { button6.text = "❌" }
             else { button6.text = "⭕" }
-            check_win()
+            check_all()
         }
         button7.setOnClickListener {
             if (button7.text == " " || button7.text == "⭕") { button7.text = "❌" }
             else { button7.text = "⭕" }
-            check_win()
+            check_all()
         }
         button8.setOnClickListener {
             if (button8.text == " " || button8.text == "⭕") { button8.text = "❌" }
             else { button8.text = "⭕" }
-            check_win()
+            check_all()
         }
         button9.setOnClickListener {
             if (button9.text == " " || button9.text == "⭕") { button9.text = "❌" }
             else { button9.text = "⭕" }
-            check_win()
+            check_all()
         }
         reset.setOnClickListener {
             button1.text = " "
@@ -122,7 +146,15 @@ class MainActivity : AppCompatActivity() {
             button7.text = " "
             button8.text = " "
             button9.text = " "
-            check_win()
+            check_all()
+        }
+
+        podval.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Приветики ^_^",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
